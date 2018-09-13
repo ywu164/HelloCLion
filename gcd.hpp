b0a81137533ea2c47293b22550b1024e84ebfece
YipWu <yipanwu21@gmail.com>
2018-09-13 15:05:25 -0700

    lab1

--- gcd.hpp
+++ gcd.hpp
@@ -1,19 +1,7 @@
-
 // Greatest Common Denominator
 // PRE : a is a positive integer
 // PRE : b is a positive integer
 // POST : a and b are unchanged
 // RETURN : the greatest common denominator of
 // a and b.
-
-int gcd ( const int a, const int b){
-    int greatest;
-    for(int i = 1; i <= a && i <= b; i++){
-
-        if(a % i==0 && b % i == 0 ){
-
-            greatest = i;
-        }
-    }
-    return greatest;
-}
+int gcd ( const int a, const int b);
--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-#include "gcd.hpp"
+
+int gcd ( const int a, const int b){
+    int greatest;
+    for(int i = 1; i <= a && i <= b; i++){
+
+        if(a % i==0 && b % i == 0 ){
+
+            greatest = i;
+        }
+    }
+    return greatest;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << gcd(36, 20) << std::endl;
